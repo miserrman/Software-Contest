3b0d1c28ae2328b3a5edb9e98736de22c4d79dd8
miserrman <41714236+miserrman@users.noreply.github.com>
2019-10-25 09:22:29 +0800

    'wz_create_chuanyi'

---
+++ dapei/src/main/java/com/garment/dapei/DapeiApplication.java
@@ -0,0 +1,13 @@
+package com.garment.dapei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DapeiApplication.class, args);
+    }
+
+}
