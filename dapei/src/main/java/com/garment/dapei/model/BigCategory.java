3f6bfe8fc62b76c1f2df00a78c1cb329ea646add
dyffff1217 <1462116324@qq.com>
2019-10-25 22:51:47 +0800

    dyf+add_class

---
+++ dapei/src/main/java/com/garment/dapei/model/BigCategory.java
@@ -0,0 +1,9 @@
+package com.garment.dapei.model;
+
+import lombok.Data;
+
+@Data
+public class BigCategory {
+    private int categoryID;
+    private String categoryInformation;
+}
---
+++ dapei/src/main/java/com/garment/dapei/model/OutFit.java
@@ -0,0 +1,11 @@
+package com.garment.dapei.model;
+
+import lombok.Data;
+
+@Data
+public class OutFit {
+    private int userID;
+    private int clothesID;
+    private int outfitID;
+    private int likeFrequency;
+}
